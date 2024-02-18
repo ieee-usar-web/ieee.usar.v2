8b8e0ec424c82c6ad751d1927d5be0fe1cc0d782
Shlok2729 <shlokgupta0727@gmail.com>
2024-02-18 16:12:19 +0530

    Loader has been intiaised

--- app/Components/Loader.jsx
+++ app/Components/Loader.jsx
@@ -1,7 +1,76 @@
-import React from "react";
-
+import React, { useEffect, useRef } from "react";
+import "./Loader.css";
+import gsap from "gsap";
 function Loader() {
-  return <div>Loader</div>;
-}
+  const starBg = useRef(null);
+  const loaderAnimation = () => {
+    let timeline = gsap.timeline();
+    timeline.to(
+      ".hover-text",
+      {
+        width: 110 + "%",
+        duration: 1.5,
+        ease: "power1.inOut",
+      },
+      0
+    );
+    timeline.to(
+      ".title",
+      {
+        x: -5 + "vw",
+        duration: 0.6,
+        opacity: 0,
+        stagger: 2,
+      },
+      2
+    );
+    timeline.to(
+      ".papaDiv",
+      {
+        y: -10 + "vh",
+        duration: 1,
+        opacity: 0,
+        stagger: 2,
+        display: "none",
+      },
+      3
+    );
+  };
 
+  useEffect(() => {
+    const starBackground = starBg.current;
+    function createStars() {
+      const numStars = 300;
+
+      for (let i = 0; i < numStars; i++) {
+        const star = document.createElement("div");
+        star.className = "star";
+        star.style.left = `${Math.random() * 100}%`;
+        star.style.top = `${Math.random() * 100}%`;
+        star.style.animationDuration = `${Math.random() * 3 + 1}s`;
+        star.style.animationDelay = `${Math.random() * 1}s`;
+        starBackground.appendChild(star);
+      }
+    }
+    createStars();
+    loaderAnimation();
+  }, []);
+  return (
+    <>
+      <div className="papaDiv">
+        <div ref={starBg} className="star-background"></div>
+        <div className="title">
+          <button data-text="Awesome" className="button">
+            <span className="actual-text">&nbsp;IEEE &amp; IEEE-W&nbsp;</span>
+            <span className="hover-text">&nbsp;IEEE &amp; IEEE-W&nbsp;</span>
+          </button>
+        </div>
+        <div class="bar">
+          <div class="circle"></div>
+          <p>Loading</p>
+        </div>
+      </div>
+    </>
+  );
+}
 export default Loader;
